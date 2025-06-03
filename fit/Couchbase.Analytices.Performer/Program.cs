8c5524883db8f1d17ac4605c75c8aa94e9a2d8c5
jmorris <jeffrymorris@gmail.com>
2025-06-02 18:09:22 -0700

    Add empty projects for FIT Performer

---
+++ fit/Couchbase.Analytices.Performer/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Couchbase.Analytices.Performer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ fit/Couchbase.Analytics.Performer.UnitTests/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Couchbase.Analytics.Performer.UnitTests;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
